51456f22f02842e3fbe210cdc39dd9aeb7108d1b
blacepo <88632326+blacepo@users.noreply.github.com>
2022-01-01 23:01:28 -0800

    Add files via upload

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const Timelock = artifacts.require('Timelock')
 const Token = artifacts.require('Token')
-const timeMachine = require('ganache-time-traveler');
+
 
 contract('Timelock&Token',() => {
 
@@ -37,12 +37,8 @@ contract('Timelock&Token',() => {
     })
 
     it('Should be able to withdraw and return empty Lock array', async () => {
-        //@notice block advancment
-        /*
-        advancement = 86400 * 10 // 10 Days
-        await timeMachine.advanceTimeAndBlock(advancement);
-        */
-        //withdrawing after time
+      
+        
         const withdrawCall = await timeLock.withdraw.call('0')
         assert(withdrawCall.length === 0)
     })
@@ -55,11 +51,7 @@ contract('Timelock&Token',() => {
         
         await timeLock.lockToken(token.address, '100', '1')
         await timeLock.lockToken(token.address, '200', '1')
-        //@notice block advancment
-        /*advancement = 86400 * 10 // 10 Days
-        await timeMachine.advanceTimeAndBlock(advancement);
-        */
-        //withdrawing after time
+        
         await timeLock.withdraw('1')
         
         const afterWithdraw = await token.balanceOf(accounts[0])
